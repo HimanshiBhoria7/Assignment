cedb4b1f3c207b3eaf70e2fbc09f38b197da1d3c
Himanshi Bhoria <himanshi.bhoria@espire.com>
2021-09-09 22:22:43 +0530

    Shapes Commit

---
+++ shapes3d/Circle.java
@@ -0,0 +1,15 @@
+package com.shapes3d;
+
+public class Circle extends TwoDimentionalShape {
+    float areaofcircle;
+	Circle(float radius){
+		super(radius);
+	}
+	
+	//For circle area
+	
+	void getArea()	{
+		areaofcircle=getDimention1()*getDimention1()*(float)Math.PI;
+		System.out.println("Area of circle : "+areaofcircle);
+	}
+}
---
+++ shapes3d/TwoDimentionalShape.java
@@ -0,0 +1,12 @@
+package com.shapes3d;
+
+public abstract class TwoDimentionalShape extends Shape {
+
+	TwoDimentionalShape(float side1){
+		super(side1);
+	}
+    TwoDimentionalShape(float side1,float side2) {
+		super(side1,side2);
+	}
+	abstract void getArea();
+}
