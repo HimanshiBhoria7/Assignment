a4bf14c81f85b50e48121148827120f1343b66fb
Himanshi Bhoria <himanshi.bhoria@espire.com>
2021-09-14 15:28:19 +0530

    Priority Queue Assignment

---
+++ priorityqueue/InsertGivenElementinPriorityQueue.java
@@ -0,0 +1,21 @@
+package com.priorityqueue;
+import java.util.PriorityQueue;
+
+public class InsertGivenElementinPriorityQueue {
+	public static void main(String args[]) {
+		PriorityQueue<String> queue = new PriorityQueue<String>();
+		queue.add("Red");
+		queue.add("Black");
+		queue.add("Orange");
+		queue.add("White");
+	
+		System.out.println("Elements are : "+queue);
+		
+		//Adding a element in queue
+		queue.offer("Yellow");
+		queue.add("Green");
+		
+		System.out.println(queue);
+		
+	}
+}
